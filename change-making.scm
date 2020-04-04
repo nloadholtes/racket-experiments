f643ae3b3afa5a09ba38912e6308d4cfc848177d
Nick Loadholtes <nick@ironboundsoftware.com>
2020-04-04 12:25:19 -0400

    Seems to work

--- change-making.scm
+++ change-making.scm
@@ -3,4 +3,20 @@
   (cond ((= coin 1) 1)
         ((= coin 2) 5)
         ((= coin 3) 10)
-        ((= coin 4) 25)))
+        ((= coin 4) 25)
+        ((= coin 5) 50)))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                    (- kinds-of-coins 1))
+                  (cc (- amount
+                    (get-coin-value kinds-of-coins))
+                      kinds-of-coins))
+          )
+  )
+)
+
+(define (count-change amount)
+  (cc amount 5))
