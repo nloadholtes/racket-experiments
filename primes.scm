3ad59cfd8d8666d0323827d8ec6f3dce3a9aa301
Nick Loadholtes <nick.loadholtes@datarobot.com>
2020-04-10 12:43:10 -0400

    regular divisor based search

--- primes.scm
+++ primes.scm
@@ -6,6 +6,22 @@
 (define (square n)
   (* n n))
 
+;; Searching for divisors 1.2.6
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+;; Fermat explorations
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
         ((even? exp)
@@ -15,7 +31,7 @@
 
 (define (fermat-test n)
   (define (try-it a)
-    (= expmod a n n) a)
+    (= (expmod a n n) a))
   (try-it (+ 1 (random (- n 1)))))
 
 (define (fast-prime? n times)
